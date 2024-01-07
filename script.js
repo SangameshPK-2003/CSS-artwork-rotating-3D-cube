42a131a96743ea1d68fc56496055a9bda33ccf66
Sangamesh Kardegouda <kardegoudasangamesh@gmail.com>
2024-01-07 20:52:00 +0530

    Delete script.js

--- script.js
+++
@@ -1,3 +0,0 @@
-document.getElementById("cube").addEventListener("click", function() {
-    this.style.animationPlayState = (this.style.animationPlayState === "running") ? "paused" : "running";
-});
